4c4a13ef2d52ae357bb1f889aff7c19b25b0e0ef
Arjun Kumar <50793512+arjun-computer-geek@users.noreply.github.com>
2022-02-01 22:34:49 +0530

    create: Avatar component created

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const header = document.querySelector('header')
 document.addEventListener('scroll', ()=>{
     var scroll_position = window.scrollY;
-    if(scroll_position > 150){
+    if(scroll_position > 100){
         header.style.boxShadow = "0px 15px 10px -15px #ffff";
         header.style.backgroundColor = "#E7E9F0"
     }else{
