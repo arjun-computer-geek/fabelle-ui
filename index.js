e20d0989941eef80631dab17212bb7f5e46cc1ee
Arjun Kumar <50793512+arjun-computer-geek@users.noreply.github.com>
2022-02-08 21:50:10 +0530

    Fix : Fixes structured file

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const header = document.querySelector('header')
 document.addEventListener('scroll', ()=>{
     var scroll_position = window.scrollY;
-    if(scroll_position > 100){
+    if(scroll_position > 20){
         header.style.boxShadow = "0px 15px 10px -15px #ffff";
         header.style.backgroundColor = "#E7E9F0"
     }else{
